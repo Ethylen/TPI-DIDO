fa6888c6779f672323c6e9e1e7872a6db8041147
Kanorii <alicedd.shadow@outlook.com>
2018-05-30 12:05:54 +0200

    connexion BD ainsi que voir cosplay ok

--- Code/WebCosplayer/model/model.php
+++ Code/WebCosplayer/model/model.php
@@ -1,7 +1,48 @@
 <?php
 /**
+ * Include.php
  * Created by PhpStorm.
  * User: tiffany.di-domenico
  * Date: 25.05.2018
  * Time: 11:28
  */
+
+
+/** ----------------------------
+ * GetBD()
+ * Created the 30.05.2018
+ * Update time :
+ * Create by TDO
+ * Comment : Connexion to the BD
+ * out : $connexion
+ * -----------------------------
+ */
+
+function getBD() {
+    //Connect to the server MySQL
+    $connexion = new PDO ('mysql:host=localhost; dbname=webcosplay_db', 'root', '');
+    // Return errors
+    $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $connexion;
+}
+
+
+/** ----------------------------
+ * GetCosplayer()
+ * Created the 30.05.2018
+ * Update time :
+ * Create by TDO
+ * Comment : Profile Data
+ * out : $resultat
+ * -----------------------------
+ */
+
+function getCosplayer() {
+    //Connect to the server MySQL
+    $connexion = getBD();
+    //Request definition
+    $req = "SELECT * FROM cosplayers";
+
+    $result = $connexion->query($req);
+    return $result;
+}
